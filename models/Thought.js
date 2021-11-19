053450bde4ddb681d05419742aa31936f865cff6
Michelle Wehr <michelle_wehr@icloud.com>
2021-11-19 12:00:54 -0500

    can add user, update user, but friends attribute is not returning correctly

---
+++ models/Thought.js
@@ -0,0 +1,59 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionBody: {
+            type: String, 
+            required: 'Input required!',
+            maxLength: 280
+        },
+        username: {
+            type: String, 
+            required: 'username required!'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+          }
+    },
+    {
+        toJSON: {
+          getters: true
+        }
+      }
+)
+
+const ThoughtSchema = new Schema (
+    {
+    thoughtText: {
+        type: String,
+        required: 'Input required!',
+        maxLength: 280
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+      },
+      username: {
+          type: String, 
+          required: 'Username required!'
+      },
+      reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+  }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller')
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router 
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+
+module.exports = router;
